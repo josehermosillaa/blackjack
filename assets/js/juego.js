296db0bc756b4aaf87b62043e66a94f6a6ba475b
josehermosilla <jose.hermosillaa@usach.cl>
2022-12-19 10:29:44 -0300

    add pedirCarta and valorCarta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,9 +6,9 @@
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K']
+//esta funcion crea una nueva baraja
 const crearDeck = () => {
-
-    for (let i = 2; i < 10; i++) {
+    for (let i = 2; i <= 10; i++) {
         for (let tipo of tipos) {
             deck.push(i + tipo);
         }
@@ -18,9 +18,34 @@ const crearDeck = () => {
             deck.push(esp + tipo);
         }
     }
-    console.log(deck);
+    // console.log(deck);
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
 }
 crearDeck();
+
+
+//esta funcion me permite tomar una carta
+//cuando yo pido una carta, si hago un pop seria la carta de arriba y no deberia existir en la baraja
+
+const pedirCarta = () => {
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop()
+    console.log(deck)
+    console.log(carta);
+    return carta;
+}
+
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor == 'A') ? 11 : 10
+        : valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
